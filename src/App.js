249502e45319c93547b498418d5b4f718438176c
omid <omidmohebbise@gmail.com>
2022-05-25 12:13:42 +0430

    sdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 import Myform from "./components/Myform";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
+      <h1>DD</h1>
       <Myform />
     </div>
   );
