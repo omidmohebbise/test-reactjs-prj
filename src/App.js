37db49fc44cb03321970985731bb47039411dc72
gairlone <106232082+gairlone@users.noreply.github.com>
2022-05-25 12:39:55 +0430

    change ccc to aaa

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>cccccc</h1>
+      <h1>aaa</h1>
+
       <Myform />
     </div>
   );
